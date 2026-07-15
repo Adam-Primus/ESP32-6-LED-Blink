b2aa9a43f05966411e9d55ab3d5453480827c627
Adam-Primus <adam0306090@gmail.com>
2026-07-15 10:19:32 +0530

    Added ESP32-code for 6-led blink

---
+++ ESP32-code.ino
@@ -0,0 +1,37 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(25, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH); 
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);                      
+  digitalWrite(12, HIGH);  
+  delay(100);
+  digitalWrite(12, LOW);
+  delay(100);
+  digitalWrite(14, HIGH);
+  delay(100);
+  digitalWrite(14, LOW);
+  delay(100);
+  digitalWrite(27, HIGH);
+  delay(100);
+  digitalWrite(27, LOW);
+  delay(100);
+  digitalWrite(26, HIGH);
+  delay(100);
+  digitalWrite(26, LOW);
+  delay(100);
+  digitalWrite(25, HIGH);
+  delay(100);
+  digitalWrite(25, LOW);
+  delay(100);
+}                    
+
